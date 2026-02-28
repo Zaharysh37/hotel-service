a42d856a7adeac2abdf8c7ee6472fb3bc142dad2
MegaXRL <zaxarrim@gmail.com>
2026-02-28 15:16:54 +0300

    db scheme

---
+++ src/main/resources/db/sql/changelog/v1.0/2026-02-26-01-apply-indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_hotels_city ON hotels(city);
+CREATE INDEX idx_hotels_country ON hotels(country);
+CREATE INDEX idx_hotels_brand ON hotels(brand);
+CREATE INDEX idx_hotels_name ON hotels(name);
+CREATE INDEX idx_hotel_amenities_amenity ON hotel_amenities(amenity);
